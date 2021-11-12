b0c62afaf1e504d6a37302fdfc20f5f930b39693
deepspacediver <luka.koscik@gmail.com>
2021-11-12 13:14:49 +0100

    initial loop creating divs with pseudocode

---
+++ READme.md
@@ -0,0 +1,8 @@
+create container for grid of divs
+create loop that takes two values
+    amount of rows and amount for columms
+    create specified amount of divs in a loop using innerHTML in a container div
+        loop is executed until number of divs = rows * columns and iterated by ++
+
+
+
--- java.js
+++ java.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const container = document.getElementById('etch-container');
+
+function createGrid(rows, columns){
+    for (let i =0; i<=(rows * columns); i++){
+        const divs = document.createElement('div');
+        divs.className = 'box';
+        container.appendChild(divs);
+    }
+}
+
+createGrid(16, 16);
